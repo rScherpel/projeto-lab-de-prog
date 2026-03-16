c3be7c63dd9bdc82550701aae0421e9b3f3c1c07
rScherpel <scherpelrafa@gmail.com>
2026-03-15 22:17:09 -0300

    Corrige import de rotas backend e instala react-router-dom no frontend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const users = require("../data/users");
+const users = require("../data/user");
 
 const router = express.Router();
 const SECRET = "segredo-super";
